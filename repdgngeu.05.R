95e8fed4f482163cebe1c6883632b9c41c1bf4cb
Sally Ploch <sally.ploch@gmail.com>
2017-10-17 18:40:09 -0500

    Update repdgngeu.05.R

--- repdgngeu.05.R
+++ repdgngeu.05.R
@@ -1,9 +1,9 @@
 WB <- read.table("/Users/sallyploch/mount/sally/compare_cohorts/FDR0.05_FHSobs_v_GTExWB.meqtl.trans.diffchr_in_DGN_GEU.txt", header=TRUE)
 repdgngeu <- select(WB, DGN_pval, GEU_pval)
-repdgngeu %>% arrange(DGN_pval)
-repdgngeu %>% arrange(GEU_pval)
-filter(repdgngeu, DGN_pval < 0.05)
-filter(repdgngeu, GEU_pval < 0.05)
+repdgngeu %>% arrange(DGN_pval) #gives denominator for dgn 
+repdgngeu %>% arrange(GEU_pval) #gives denominator for geu
+filter(repdgngeu, DGN_pval < 0.05) #gives numerator for dgn
+filter(repdgngeu, GEU_pval < 0.05) #gives numerator for geu
 
 
 
